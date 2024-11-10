1641af1a6b4037c602731f37e3cfccd0c5355a8e
OnesmusBett <onesmuskipchumba5@gmail.com>
2024-11-10 09:59:06 +0300

    Configured database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
 import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-
-dotenv.config();
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
+  connectDB();
 });
